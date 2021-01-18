ff6aba9e209304ba8af37f7ac4f11f1f8ac45c62
Gianpaolo <pao.barga@gmail.com>
2021-01-18 02:31:36 +0100

    bonus: aggiunto validazione input duplicati e aggiornato alcune funzioni

--- bonus/js/script-bonus.js
+++ bonus/js/script-bonus.js
@@ -1,12 +1,17 @@
+/* NOTES:
+The choice of having three different functions validating the input depends on the fact that I wanted to practice with functions; normally I would write only one function.
+*/
+
 // ---- GAME'S PARAMETERS
 //how many numbers
 var difficulty = 5;
 //waiting time (in milliseconds)
-var time = 30000;
+var time = 1000;
 //arr containing random numbers
 var randomNumbers = [];
 
 
+// ---- FUNCTIONS
 //function which creates an array of n numbers with no duplicates; it returns an array
 function createRandomNumbers(howMany, min, max) {
     var result = []; //array which will contain the random numbers
@@ -25,26 +30,50 @@ function createRandomNumbers(howMany, min, max) {
     return result;
 }
 
-// ---- FUNCTIONS
-//function: get input from the user and check whether it is valid; it loops until the input is valid; it returns the input.
-function getCheckInputs() {
-    var check = false; //flag (initialization)
-    var message = "Inserisci uno dei numeri mostrati in precedenza:" //message to get the input
-
-    while(!check) {
-        //getting the  input
-        userInput = parseInt(prompt(message));
-
-        //checking if the input is valid
-        if(!isNaN(userInput)) {
-            check = true;
-        }
+// function: check the input from the user. It returns a boolean.
+function checkInput(value) {
+        return !isNaN(value);
     }
     
+// function: check whether the input is a duplicate; it push the input into an array if it is not a duplicate; it returns a boolean.
+var userInputsArray = []; //array containing the user inputs (used only by the fn checkUserDuplicates)
+function checkUserDuplicates(value) {
+        
+        if(!userInputsArray.includes(value)) {
+            userInputsArray.push(value);
+            
+            return true;
+        }
+
+    return false;
+}
+
+// function: get the input from the user and pass it through the checkInput fn and checkUserUpdate; it loops until the input is not correct; it returns the input.
+function getInput() {
+    var userInput = 0; // user input (initialization)
+    var check = false; //flag (initialization)
+    var message = "Inserisci uno dei numeri visti in precedenza";
+
+    userInput = parseInt(prompt(message));
+
+    while(!check) {
+        // input validation
+        if(!checkInput(userInput)) {
+            userInput = parseInt(prompt("Il numero inserito non è valido. Sono accettati solo numeri interi da 1 a 100"));
+        } else {
+            // is the inpute a duplicate?
+            if(!checkUserDuplicates(userInput)) {
+                userInput = parseInt(prompt("Hai già inserito questo numero. Riprova"));
+            } else {
+                check = true;
+            }
+        }
+    }
+
     return userInput;
 }
 
-//function: victory message
+// function: victory message
 function victoryMessage(userNumbersArray, randomNumbersArray) {
     var message = ""; //victory message (initialization)
     var lengthUserArray = userNumbersArray.length;
@@ -61,7 +90,7 @@ function victoryMessage(userNumbersArray, randomNumbersArray) {
     return message;
 }
 
-//function: game
+// function: game
 //???? dovrei passare le altre funzioni come argomenti o va bene prenderle da fuori ????
 function game(randomNumbersArray) {
     var userNumber = 0; //number from the user (initialization)
@@ -70,7 +99,7 @@ function game(randomNumbersArray) {
     for(var i = 0; i < randomNumbersArray.length; i++) {
 
         // get the user numbers and check
-        userNumber = getCheckInputs();
+        userNumber = getInput();
 
         if(randomNumbersArray.includes(userNumber)) {
             guessedNumbers.push(userNumber);
@@ -79,6 +108,7 @@ function game(randomNumbersArray) {
 
     alert(victoryMessage(guessedNumbers, randomNumbersArray));
 }
+
 
 // ---- SCRIPT
 //???? Come posso chiamare questa sezione in cui tutto viene eseguito????
@@ -90,4 +120,6 @@ console.log(randomNumbers); //a little help for the developer (useless for the s
 alert(`Leggi e memorizza i numeri seguenti: ${randomNumbers.join(", ")}.`);
 
 //get the user answer after a certain amount of time, evaluate the answers and give back the result
-setTimeout(game(randomNumbers), time);
+setTimeout(function(){
+    game(randomNumbers);
+}, time);
